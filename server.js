d83542f0d2985c0c4898e9bc3e4a36529258bc92
Alexandre C <seafoox@gmail.com>
2012-02-28 14:15:51 +0100

    initial version v0.1

---
+++ server.js
@@ -0,0 +1,19 @@
+var app = require('./app').init(4000);
+
+var locals = {
+        title: 		 'NodeJS Boostrap using Express / EJS / Twitter Bootstrap / CSS3',
+        description: 'Node Express HTML5 & CSS3',
+        author: 	 'Alexandre Collin'
+    };
+
+app.get('/', function(req,res){
+
+    locals.date = new Date().toLocaleDateString();
+
+    res.render('template.ejs', locals);
+});
+
+/* The 404 Route (ALWAYS Keep this as the last route) */
+app.get('/*', function(req, res){
+    res.render('404.ejs', locals);
+});
